5e018047b5559f72c4826b5f2a87103a9da844f5
Christian Medina <christianmedina121@hotmail.com>
2020-03-20 18:46:59 -0400

    Adding source files

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	
+	public static void main(String args[]){
+		
+	}
+	
+	public void humanTurn(){
+		
+	}
+	
+	public void compTurn(){
+		
+	}
+	
+	public void numCases(int num){
+		
+	}
+	
+	public boolean win(){
+		return false;
+	}
+
+}
